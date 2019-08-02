d29eb3e8c1b32fbf7000afc130850af8cedacd8d
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-02 22:39:03 +0700

    tugas_13_js
    tugas sudah ok !!!

---
+++ tugas-13.js
@@ -0,0 +1,11 @@
+function panggilbuah(){
+
+  var buah = ["Pisang", "Jeruk"];
+  console.log(buah);
+  console.log("");
+  buah.unshift("Apel", "Mangga");
+
+  return buah;
+}
+
+console.log(panggilbuah());
